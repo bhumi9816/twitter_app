31a96538b0603061acd0af6828917cc92c33f131
Bhumi Patel <bhumiv1999@yahoo.com>
2021-03-04 21:36:43 -0800

    twitter-app first commit

---
+++ Twitter/LoginViewController.swift
@@ -0,0 +1,59 @@
+//
+//  LoginViewController.swift
+//  Twitter
+//
+//  Created by Bhumi Patel on 3/2/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        if UserDefaults.standard.bool(forKey: "userLoggedIn") == true {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }
+    }
+    
+    @IBAction func onLoginButton(_ sender: Any) {
+        
+        //using TwitterAPICaller method to call the client method for login where: we specify which url [for login] to use, and what if login is success and what if login is failure...
+        
+        let myUrl = "https://api.twitter.com/oauth/request_token"
+        
+        TwitterAPICaller.client?.login(url: myUrl, success: {
+            
+            UserDefaults.standard.set(true, forKey: "userLoggedIn")
+            
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+            
+        }, failure: { (Error) in
+            print("Could not Login in!!")
+        })
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
